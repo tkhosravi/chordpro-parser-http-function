29f1b343033f7a9a61601c473e06bcc2b2c05c49
Tony Khosravi Dehkourdi <tkhosravi@users.noreply.github.com>
2019-04-21 17:42:23 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+const chordsheetjs = require('chordsheetjs');
+
+/**
+ * HTTP Cloud Function.
+ *
+ * @param {Object} req Cloud Function request context.
+ *                     More info: https://expressjs.com/en/api.html#req
+ * @param {Object} res Cloud Function response context.
+ *                     More info: https://expressjs.com/en/api.html#res
+ */
+exports.helloHttp = (req, res) => {
+  const name =
+    req.query && req.query.name
+      ? req.query.name
+      : req.body && req.body.name
+      ? req.body.name
+      : 'World';
+  res.send(`Hello ${escapeHtml(name)}!`);
+};
