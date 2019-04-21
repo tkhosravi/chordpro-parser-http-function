81cdd6e494f0c27430626c17fc321015abcb4dae
Tony Khosravi Dehkourdi <tkhosravi@users.noreply.github.com>
2019-04-21 18:48:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,12 +8,8 @@ const ChordSheetJS = require('chordsheetjs');
  * @param {Object} res Cloud Function response context.
  *                     More info: https://expressjs.com/en/api.html#res
  */
-exports.helloHttp = (req, res) => {
-  const name =
-    req.query && req.query.name
-      ? req.query.name
-      : req.body && req.body.name
-      ? req.body.name
-      : 'World';
-  res.send(`Hello ${ChordSheetJS(name)}!`);
+exports.parseChordpro = (req, res) => {
+  const chordSheet = req.body;
+  const parser = new ChordSheetJS.ChordProParser();
+  res.send(parser.parse(chordSheet));
 };
